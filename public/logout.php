a52ab9be3319334fac53dde09fe5caebe376e3e5
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-09-21 11:17:37 -0300

    add

--- public/logout.php
+++ public/logout.php
@@ -1,30 +1,13 @@
 <?php
 session_start();
 
-// Limpa todas as variáveis da sessão atual
+// Destrói todas as variáveis de sessão
 $_SESSION = array();
 
-// Se for utilizado cookie de sessão, elimina o cookie no navegador do utilizador
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(),
-        '',
-        time() - 42000,
-        $params["path"],
-        $params["domain"],
-        $params["secure"],
-        $params["httponly"]
-    );
-}
-
-// Destrói completamente a sessão
+// Destrói a sessão no servidor
 session_destroy();
 
-// Inicia uma nova sessão apenas para mandar a mensagem de confirmação
-session_start();
-$_SESSION['mensagem_sucesso'] = "Sessão encerrada com sucesso!";
-
-// Redireciona para a página de login
-header('Location: login.php');
-exit();
+// Redireciona para o login
+header("Location: login.php");
+exit;
+?>
